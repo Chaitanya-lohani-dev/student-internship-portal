b451f5f431f29987e45f6533fb3a82783ec75fe6
AloneFFOfficial <chaitanyalohani175@gmail.com>
2026-01-25 16:29:13 +0530

    fix(auth): fixed and checked if all auth ui redirects and fuctionalities are working.

--- frontend/app/login/page.jsx
+++ frontend/app/login/page.jsx
@@ -1,7 +1,9 @@
 'use client';
-import { useState, useEffect} from 'react'
+import { useState } from 'react'
 import { z } from 'zod';
 import { loginAPI } from '../lib/api.ts';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const loginSchema = z.object({
   email: z.string().email(),
@@ -10,36 +12,57 @@ const loginSchema = z.object({
 
 export default function Login() {
   const [state, setState] = useState(null)
+  const [loading,setLoading] = useState(false);
+  const [success,setSuccess] = useState(false);
+  const [showPassword, setShowPassword] = useState(true);
+
+  const router = useRouter();
+
   const handelSubmit = async(e) => {
-    e.preventDefault()
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData.entries());
-    const validation = loginSchema.safeParse(data);
-
-    if (!validation.success) {
-        setState(<div className='bg-red-600 text-white'>Invalid Data Please try again</div>)
+    try {
+      e.preventDefault()
+      setLoading(true)
+      const formData = new FormData(e.target);
+      const data = Object.fromEntries(formData.entries());
+      const validation = loginSchema.safeParse(data);
+  
+      if (!validation.success) {
+        setState('Invalid Data Please try again')
         return;
-    }
-    const { email, password} = validation.data;
-    const res = await loginAPI(email, password);
-
-    if (res === "loginSuccess"){
-      setState(<div className='text-white bg-green-600'> Login SuccessFull</div>)
-    } else {
-      setState(<div className='text-white bg-red-600'> Error Trying to Login</div>)
+      }
+  
+      const { email, password} = validation.data;
+      const res = await loginAPI(email, password);
+  
+      if (res === "loginSuccess"){
+        setSuccess(true)
+        setState('Login successful')
+        setTimeout(() => router.push('/student/jobs'), 2000)
+      } else {
+        setState('Error Trying to Login')
+      }
+    } finally {
+      setLoading(false)
     }
   }
   
   return (
-    <div>
-      <form onSubmit={handelSubmit}>
-        <label>Email</label>
-        <input type='email' name='email'required></input>
-        <label>Password</label>
-        <input type='password' name='password'required></input>
-        <button type='submit'>Submit</button>
+    <div className='m-5 p-5 border-2 border-black w-120 justify-items-center bg-blue-500 text-white'>
+      <form onSubmit={handelSubmit} className='flex flex-col'>
+        <div className='flex flex-row font-bold'><label>Email</label><p className='text-red-600'>*</p></div>
+        <input type='email' name='email' required className='block border'></input>
+        <div className='flex flex-row font-bold'><label>Password</label><div className='text-red-600'>*</div></div>
+        <div className='flex flex-row'><input type={showPassword ? "password": 'text'} name='password' required className='border'></input> <button type='button'onClick={()=> setShowPassword(!showPassword)}>👁️</button></div>
+        <button type='submit' className='block m-2 p-2 bg-blue-600' disabled={loading}>{loading ? "submitting..." : 'submit'}</button>
       </form>
-      {state}
+      {state!==null &&
+      <div className={success ? "bg-green-700 text-white": 'bg-red-700 text-white'}>{state}</div>}
+      <div className='flex justify-between space-x-2'>
+        <div>
+          Don't Have a account?
+        </div>
+        <Link href={'/register'} className='font-bold'>Register Now</Link>
+      </div>
     </div>
   )
 }
--- frontend/app/register/page.jsx
+++ frontend/app/register/page.jsx
@@ -3,6 +3,7 @@ import { useState} from 'react'
 import { useRouter } from 'next/navigation';
 import { z } from 'zod';
 import { registerAPI } from '../lib/api.ts';
+import Link from 'next/link';
 
 const registerSchema = z.object({
     name: z.string().min(3),
@@ -11,46 +12,64 @@ const registerSchema = z.object({
 });
 
 export default function Register() {
+  const [success, setSuccess] = useState(false);
+  const [loading,setLoading] = useState(false);
   const [state, setState] = useState(null);
+  const [showPassword, setShowPassword] = useState(true);
+
   const router = useRouter();
 
   const handelSubmit = async(e) => {
-    e.preventDefault()
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData.entries());
-    const validation = registerSchema.safeParse(data);
-
-    if (!validation.success) {
-        setState(<div className='bg-red-600 text-white'>Invalid Data Please try again</div>);
+    try {
+      e.preventDefault()
+      setLoading(true)
+      const formData = new FormData(e.target);
+      const data = Object.fromEntries(formData.entries());
+      const validation = registerSchema.safeParse(data);
+  
+      if (!validation.success) {
+        setState('Invalid Data Please try again');
         return;
-    }
-
-    const { name, email, password} = validation.data;
-
-    const res = await registerAPI(name, email, password);
-
-    if (res.message === "User Registered"){
-      setState(<div className='text-white bg-green-600'>User Registered Successfullly</div>)
-    }else if (res.message === 'User all ready exists') {
-      setState(<div className='bg-red-600 text-white font-bold'>User Already Exist please Login</div>)
-      setTimeout(() => router.push("/login"), 2000)
-    } else {
-      setState(<div className='text-white bg-red-600'> Error Trying to Register</div>)
+      }
+  
+      const { name, email, password} = validation.data;
+  
+      const res = await registerAPI(name, email, password);
+  
+      if (res.message === "User Registered"){
+        setSuccess(true)
+        setState("User Registered Successfullly")
+      }else if (res.message === 'User all ready exists') {
+        setState("User Already Exist please Login")
+        setTimeout(() => router.push("/login"), 2000)
+      } else {
+        setState('Error Trying to Register')
+      }
+    } finally {
+      setLoading(false)
     }
   }
   
   return (
-    <div>
-      <form onSubmit={handelSubmit}>
-        <label>Name</label>
-        <input type='text' name='name'required></input>
-        <label>Email</label>
-        <input type='email' name='email'required></input>
-        <label>Password</label>
-        <input type='password' name='password'required></input>
-        <button type='submit'>Submit</button>
-      </form>
-      {state}
+    <div className='w-full h-full m-0 p-0 justify-items-center'>
+      <div className='border m-2 p-2 w-auto'>
+        <form onSubmit={handelSubmit} className='flex flex-col'>
+          <div className='flex flex-row space-x-1'><label>Name</label><p className='text-red-600 space-x-1'>*</p></div>
+          <input type='text' name='name'required className='border'></input>
+          <div className='flex flex-row space-x-1'><label>Email</label><p className='text-red-600 space-x-1'>*</p></div>
+          <input type='email' name='email'required className='border'></input>
+          <div className='flex flex-row space-x-1'><label>Password</label><p className='text-red-600 space-x-1'>*</p></div>
+          <div className='flex flex-row'><input type={showPassword ?'password': 'text'} name='password'required className='border'></input><button type='button' onClick={() => setShowPassword(!showPassword)}>👁️</button></div>
+          <button type='submit' className='w-full bg-red-700 text-white font-bold gap-y-2' disabled={loading} >{loading ? "submiting..." : 'Submit'}</button>
+        </form>
+        { state !==null &&
+        <div className={success ? "w-full bg-green-700 text-white" : 'w-full bg-red-700 text-white'}>{state}</div>
+        }
+        <div>
+          <p>Already have a account?</p> <Link href="/login"className='font-bold text-red-700'>Login</Link>
+        </div>
+        </div>
     </div>
   )
 }
+
