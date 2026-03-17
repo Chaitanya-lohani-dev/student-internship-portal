38ddf935c63bcfb2d5b8fb03d6834c62f93e6feb
AloneFFOfficial <chaitanyalohani175@gmail.com>
2026-03-17 18:22:50 +0530

    feat: configure dotenv and add error handling middleware

---
+++ backend/middleware/errorHandler.middleware.js
@@ -0,0 +1,6 @@
+
+export const errorHandler = (err, _req, res, _next) => {
+    console.error(err.stack);
+    const status = err.status || err.statusCode || 500;
+    res.status(status).json({ message: err.message || "Internal Server Error" });
+}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
+import './config/env.config.js';
 import express from "express";
 import connectDB from "./config/db.js";
-import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import adminRoutes from './routes/admin.route.js';
@@ -8,24 +8,29 @@ import studentRoutes from './routes/student.route.js'
 import client from "./config/redis.js";
 import { rateLimiter } from "./middleware/rateLimiter.middleware.js";
 import cors from 'cors';
+import { errorHandler } from "./middleware/errorHandler.middleware.js";
 
-dotenv.config()
-connectDB();
+(async () => {
+    await connectDB();
+})();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({origin: `${process.env.ALLOWED_ORIGINS}` ||"http://localhost:3000", credentials: true}));
-app.use(rateLimiter(client));
-app.use('/api/auth', authRoutes)
-app.use('/api/admin', adminRoutes)
-app.use('/api/student', studentRoutes)
+app.use(cors({origin: process.env.ALLOWED_ORIGINS ||"http://localhost:3000", credentials: true}));
 
 app.get("/health", (_, res) => {
     res.status(200).json({message: 'all Systems normal'})
 })
+
+app.use(rateLimiter(client));
+app.use('/api/auth', authRoutes)
+app.use('/api/admin', adminRoutes)
+app.use('/api/student', studentRoutes)
+app.use(errorHandler);
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
